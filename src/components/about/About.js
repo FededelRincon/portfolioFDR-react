9e085dbe83e336b0efdc3f6a387111d9472ebb6a
FDR <delrincon@gmail.com>
2022-05-19 00:27:02 -0300

    favicon OK + about css done, need text + slider css done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,51 +17,38 @@ const About = () => {
                         <>
                             <h3>{`< About Me >`}</h3>
                             <p>
-                                Soy autodidacta, me siento mas comodo en la parte front-end, aunque creo 
-                                poder aportar en ambos lados del desarrollo ya que cuento con un poco de 
-                                conocimiento pero tambien con muchas ganas de seguir aprendiendo. Me apasiona 
-                                crear soluciones innovadoras con el objetivo de brindar experiencias de usuario 
-                                fluidas, manteniendo una alta funcionalidad y prestando atención a los detalles. 
-                                Me encanta la organización y la estructura y, por lo tanto, pongo un gran énfasis 
-                                en hacer un código limpio, elegante y de facil lectura. 
-                                <a 
+                                I am self-taught, but I feel more comfortable in the front-end than the back-end, but I think I can contribute to both sides of the development process because I have some knowledge in both sides and i have a lot of desire to continue learning. I create innovative solutions with the goal of delivering seamless user experiences while maintaining high functionality and paying attention to passionate detail. I love organization and structure and therefore. I place a great emphasis on making code clean, elegant and easy to read.
+                                <span 
                                     className="about-link"
                                     onClick={ () => handleReadMore(false) } >
                                     {` Read more...`}
-                                </a>
+                                </span>
                             </p>
                         </>
                     ) : (
-                        <p>
-                            Nací y crecí en Argentina. Siempre fui un fan de la tecnologia en general, tanto asi 
-                            que por el 2009 comence mis estudios en Famaf donde permaneci casi 4 años pero por motivos 
-                            economicos y el dia a dia tuve que abandonar, la vida me llevo a estudiar la carrera de 
-                            corredor inmobiliario a distancia, pero si bien llege a recibirme y trabajar de ello un tiempo 
-                            supe desde el comienzo que no es algo en lo que podia verme trabajando.
+                        <>
+                            <h3>{`< About Me >`}</h3>
+                            <p>
+                                I was born and raised in Argentina. I was always a fan of technology, so much so that in 2009 I started my studies at Famaf where I studied for almost 4 years but for economic reasons and day to day I had to leave. Life led me to study the real estate broker career remotely, but even though I got to graduate and work on it for a while, I knew from the beginning that it is not something I could see myself working on.
 
-                            Entonces, cuando la pandemia golpeó por primera vez y la vida se desaceleró, me topé con 
-                            algunos cursos en línea y al instante me decidí que quería aprender a programar pero esta 
-                            vez buscando una oportunidad real de trabajar de esto, por lo que comence con cursos, el 
-                            primero fue python, y despues pase por HTML, CSS, JS para llegar a cosas mas desafiantes 
-                            como react, Next, Express, Mongo, entre otros
-
-                            No creo tener grandes pasatiempos para compartir, aunque posiblemente aprender cosas de 
-                            forma autodidacta se podria decir q es lo mio, por nombrar algunas cosas: tocar la guitarra, 
-                            programar, conducir, aprender ingles, entre otros.
-                            <a 
-                                className="about-link"
-                                onClick={ () => handleReadMore(true) } >
-                                {` Go back`}
-                            </a>
-                        </p>
+                                So when the pandemic first hit and life slowed down I stumbled upon some online courses and instantly decided I wanted to learn programming but this time looking for a real working opportunity so I started with courses. . The first course was python, and then I went through HTML, CSS, JS to get to more challenging things like react, Next, Express, Mongo, among others.
+                                I don't think I have great hobbies to share, although possibly learning things self-taught could be said to be my thing, to name a few things: playing the guitar, programming, driving, learning English, among others.
+                                <span 
+                                    className="about-link"
+                                    onClick={ () => handleReadMore(true) } >
+                                    {` Go back`}
+                                </span>
+                            </p>
+                        </>
                     )
                 }
                 
             </div>
             <div className="about-img">
                 <img
-                src="https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg"
-                alt="about"
+                    className="img-circular"
+                    src="https://res.cloudinary.com/dqh7edrn7/image/upload/v1652927127/Portfolio/Yo_BN_omkcml.jpg"
+                    alt="about"
                 />
             </div>
         </div>
